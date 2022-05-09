863f745d6052b3e0681036b973ddc35aaefcc63f
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-05-09 19:14:37 -0400

    adding routes folders

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,21 @@
-// const router = require('express').Router();
-// const userRoutes = require('./userRoutes');
-// const projectRoutes = require('./projectRoutes');
+const router = require('express').Router();
+const userRoutes = require('./userRoutes.js');
+const apiRoutes = require('./api/');
 
-// router.use('/users', userRoutes);
-// router.use('/projects', projectRoutes);
+const homeRoutes = require('./homeRoutes.js');
+const dashboardRoutes = require('./dashboardRoutes.js');
 
-// module.exports = router;
+
+//create these two
+const postRoutes = require('./postRoutes.js');
+const commentRoutes = require('./commentRoutes');
+
+router.use('/users', userRoutes);
+router.use('/posts', projectRoutes);
+router.use('/comments', projectRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
+
+module.exports = router;
+
+
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,53 +1,52 @@
-// const router = require('express').Router();
-// const { Project, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const router = require('express').Router();
+// const { User, Post, Comment } = require('../models'); //create one file each for these three 
+const withAuth = require('../utils/auth');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     // Get all projects and JOIN with user data
-//     const projectData = await Project.findAll({
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
 //       include: [
 //         {
 //           model: User,
 //           attributes: ['name'],
 //         },
 //       ],
-//     });
-
+    });
 //     // Serialize data so the template can read it
-//     const projects = projectData.map((project) => project.get({ plain: true }));
-
+    const post = postData.map((post) => post.get({ plain: true }));
 //     // Pass serialized data and session flag into template
-//     res.render('homepage', { 
-//       projects, 
-//       logged_in: req.session.logged_in 
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('homepage', { 
+      post, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// router.get('/project/:id', async (req, res) => {
-//   try {
-//     const projectData = await Project.findByPk(req.params.id, {
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
 //       include: [
 //         {
 //           model: User,
 //           attributes: ['name'],
+//          include comments in this chunk
 //         },
 //       ],
-//     });
+    });
 
-//     const project = projectData.get({ plain: true });
+    const post = postData.get({ plain: true });
 
-//     res.render('project', {
-//       ...project,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // Use withAuth middleware to prevent access to route
 // router.get('/profile', withAuth, async (req, res) => {
@@ -76,7 +75,7 @@
 //     return;
 //   }
 
-//   res.render('login');
+//   res.render('login'); //will poiint to login.handlebars file in views 
 // });
 
 // module.exports = router;
