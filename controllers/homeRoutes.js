f4968ed564646307194afebcc231469db96f6fd5
Sarah Gilbreath <thenoiseinspace@gmail.com>
2022-05-16 09:32:11 -0400

    handlebars is connected

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,20 +9,20 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-    // Serialize data so the template can read it
-    const post = postData.map((post) => post.get({ plain: true }));
+    // const postData = await Post.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
+    // // Serialize data so the template can read it
+    // const post = postData.map((post) => post.get({ plain: true }));
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-        post, 
-      logged_in: req.session.logged_in 
+      //   post: post, 
+      // logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
+const app = express();
+const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-// // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+
 
 const sess = {
   secret: 'Super secret secret',
@@ -25,6 +25,8 @@ const sess = {
 };
 
 app.use(session(sess));
+// // Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
 // // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
